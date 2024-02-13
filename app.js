44a5b6a00cb26ae4a2c29a180da8822a9d645118
Juan C. Henao Hernandez <juanhenao123kl@gmail.com>
2024-02-12 20:46:34 -0500

    login nodejs

--- app.js
+++ app.js
@@ -1,33 +1,138 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.urlencoded({extended:false}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-const dontenv = require('dotenv')
-dontenv.config({path:'./env/.env'})
+const dontenv = require("dotenv");
+dontenv.config({ path: "./env/.env" });
 
-app.use('/resource', express.static('public'))
-app.use('/resource', express.static(__dirname + '/public'))
+app.use("/resource", express.static("public"));
+app.use("/resource", express.static(__dirname + "/public"));
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-const bcryptjs = require('bcryptjs');
+const bcryptjs = require("bcryptjs");
 
-const session = require('express-session');
-app.use(session({
-    secret: 'secret',
+const session = require("express-session");
+app.use(
+  session({
+    secret: "secret",
     resave: true,
-    saveUninitialized: true
-}))
+    saveUninitialized: true,
+  })
+);
 
 // conexion DB
-const connection = require('./database/db.js')
+const connection = require("./database/db.js");
 
-app.get('/', (req, res) => {
-    res.send('Hola Mundo');
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  const user = req.body.user;
+  const name = req.body.name;
+  const rol = req.body.rol;
+  const pass = req.body.pass;
+  const passwordHaash = await bcryptjs.hash(pass, 8);
+  connection.query(
+    "INSERT INTO users SET ?",
+    { user: user, name: name, rol: rol, pass: passwordHaash },
+    async (error, result) => {
+      if (error) {
+        console.log(error);
+      } else {
+        res.render("register", {
+          alert: true,
+          alertTitle: "Registration",
+          alertMessage: "¡Successful Registration!",
+          alertIcon: "success",
+          showConfirmButton: false,
+          timer: 1500,
+          ruta: "",
+        });
+      }
+    }
+  );
+});
+
+app.post("/auth", async (req, res) => {
+  const user = req.body.user;
+  const pass = req.body.pass;
+  const passwordHaash = await bcryptjs.hash(pass, 8);
+  if (user && pass) {
+    connection.query(
+      "SELECT * FROM users WHERE user = ?",
+      [user],
+      async (error, results) => {
+        if (
+          results.length == 0 ||
+          !(await bcryptjs.compare(pass, results[0].pass))
+        ) {
+          res.render("login", {
+            alert: true,
+            alertTitle: "Error",
+            alertMessage: "USUARIO y/o PASSWORD incorrectas",
+            alertIcon: "error",
+            showConfirmButton: true,
+            timer: false,
+            ruta: "login",
+          });
+        } else {
+          req.session.loggedin = true;  
+          req.session.name = results[0].name;
+          res.render("login", {
+            alert: true,
+            alertTitle: "Conexion exitosa",
+            alertMessage: "¡LOGIN CORRECTO!",
+            alertIcon: "success",
+            showConfirmButton: false,
+            timer: 1500,
+            ruta: "",
+          });
+        }
+      }
+    );
+  } else {
+    res.render("login", {
+      alert: true,
+      alertTitle: "Advertencia",
+      alertMessage: "¡Por favor ingrese un usuario y/o password!",
+      alertIcon: "warning",
+      showConfirmButton: true,
+      timer: false,
+      ruta: "login",
+    });
+  }
+});
+
+// Auth pages
+app.get('/', (req, res ) => {
+    if (req.session.loggedin) {
+        res.render('index', {
+            login:true,
+            name: req.session.name
+        })
+    }else {
+        res.render('index', {
+            login: false,
+            name: 'Debe iniciar sesión'
+        })
+    }
+})
+
+// Logout
+app.get('/logout', (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/')
+    })
 })
 
 app.listen(3000, (req, res) => {
-    console.log('Server running in http://localhost3000');
-})
+  console.log("Server running in http://localhost:3000");
+});
